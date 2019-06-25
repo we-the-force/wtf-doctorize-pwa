d6dea83d01c1a33187b29e29f9aaa3581ed29a44
srlechuga18 <aledeleon222@hotmail.com>
2019-06-25 16:53:50 -0500

    consultorios

--- src/js/routes.js
+++ src/js/routes.js
@@ -16,6 +16,10 @@ import AsistentesInvitar02 from '../pages/asistentes-invitar-02.f7.html';
 import AsistentesInvitar03 from '../pages/asistentes-invitar-03.f7.html';
 import AsistentesInvitar04 from '../pages/asistentes-invitar-04.f7.html';
 import AsistentesConfirmar from '../pages/asistentes-confirmar.f7.html';
+import officesShow from '../pages/offices-show.f7.html';
+import officesAddFormBasic from '../pages/offices-add-form-basic.f7.html';
+import officesAddFormMap from '../pages/offices-add-form-map.f7.html';
+//import officesAddFormSchedule from '../pages/offices-add-form-schedule.f7.html';
 
 import LeftPage1 from '../pages/left-page-1.f7.html';
 import LeftPage2 from '../pages/left-page-2.f7.html';
@@ -53,6 +57,26 @@ var routes = [
         path: '/account/signup/confirmation',
         component: RegisterS5Page,
     },
+    {
+        name: 'offices-show',
+        path: '/offices-show',
+        component: officesShow,
+    },
+    {
+        name: 'offices-add-form-basic',
+        path: '/offices-add-form-basic',
+        component: officesAddFormBasic,
+    },
+    {
+        name: 'offices-add-form-map',
+        path: '/offices-add-form-map',
+        component: officesAddFormMap,
+    },
+    /* {
+        name: 'offices-add-form-schedule',
+        path: '/offices-add-form-schedule',
+        component: officesAddFormSchedule,
+    }, */
     {
         name: 'intro-splash-01',
         path: '/intro-splash-01',

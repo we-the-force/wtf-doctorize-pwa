43d00c9a323c64e6f2e0319924dbbc590a01bd38
srlechuga18 <aledeleon222@hotmail.com>
2019-12-16 16:19:15 -0600

    cookieees

--- src/js/app.js
+++ src/js/app.js
@@ -143,6 +143,26 @@ var app = new Framework7({
     flow: 'default' //RA registrar asistente, RD registrar doctor, RP registrar paciente, UA update asistente, UD update doctor, UP update patient
 });
 
+if(getCookie("landingPage") != 'visited') {
+  app.methods.redirectTo('intro-splash-01');
+}
+
+function getCookie(cname) {
+  var name = cname + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for(var i = 0; i <ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
 app.init(function() {
     console.log('init');
 
